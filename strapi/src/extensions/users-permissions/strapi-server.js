54b1f76ef34a3677e9cfb5d8bd3099a6199bd5de
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2022-03-27 22:45:54 +0530

    updated server

--- strapi/src/extensions/users-permissions/strapi-server.js
+++ strapi/src/extensions/users-permissions/strapi-server.js
@@ -19,19 +19,10 @@ module.exports = (plugin) => {
     const user = await strapi.entityService.findOne(
       "plugin::users-permissions.user",
       ctx.state.user.id,
-      { populate: ["admin"] }
+      { populate: ["role"] }
     );
 
-    const { admin, ...rest } = sanitizeOutput(user);
-
-    ctx.body = {
-      ...sanitizeOutput(admin),
-      user: {
-        data: {
-          ...rest,
-        },
-      },
-    };
+    ctx.body = sanitizeOutput(user);
   };
 
   plugin.controllers.user.find = async (ctx) => {
